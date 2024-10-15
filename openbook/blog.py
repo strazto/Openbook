2ddccb24439a980072236d56dc117ef4395258eb
Damian-Siderides <siderides@live.com.au>
2024-10-16 00:26:57 +1100

    Added Comments Feature

--- openbook/blog.py
+++ openbook/blog.py
@@ -10,17 +10,90 @@ bp = Blueprint('blog', __name__)
 
 # if g.user:
 
-@bp.route('/blog')
+@bp.route('/blog', methods=('GET', 'POST'))
 @login_required
 def index():
+# select * from comment where post_id is x ORDER BY created DESC
+    if request.method == 'POST':
+        comment = request.form['comment']
+        post_id = request.form['post_id']
+        error = None
+
+        if not comment:
+            error = 'Comment is required.'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO comment (body, author_id, post_id)'
+                ' VALUES (?, ?, ?)',
+                (comment, g.user['id'], post_id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
+
+    # comments = db.execute(
+    #     'SELECT c.id, body, created, author_id, username'
+    #     ' FROM comment c JOIN user u ON c.author_id = u.id'
+    #     ' ORDER BY created DESC'
+    # ).fetchall()
+
+    length = db.execute('select count(*) from post').fetchone()[0]
+
+    listofcomments = [''] * (length + 1)
+    # listofcomments = []
+
+    for i in range(length + 1):
+        listofcomments[i] = db.execute(
+                f'select c.id, c.body, c.created, c.author_id, post_id, username from comment c join user u on c.author_id = u.id where post_id is {i}'
+            ).fetchall()
+
+    return render_template('blog/index.html', posts=posts, listofcomments=listofcomments)
+
+@bp.route('/blog/post/<int:id>', methods=('GET', 'POST'))
+def post(id):
+
+    if request.method == 'POST':
+        comment = request.form['comment']
+        post_id = request.form['post_id']
+        error = None
+
+        if not comment:
+            error = 'Comment is required.'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO comment (body, author_id, post_id)'
+                ' VALUES (?, ?, ?)',
+                (comment, g.user['id'], post_id)
+            )
+            db.commit()
+            # return redirect(url_for('blog.index.post.id'))
+            return redirect(f'/blog/post/{id}')
+
+    posts = [get_post(id, check_author=False)]
+
+    db = get_db()
+
+    length = db.execute('select count(*) from post').fetchone()[0]
+
+    listofcomments = [None] * (length + 1)
+
+    listofcomments[id] = db.execute(
+                f'select c.id, c.body, c.created, c.author_id, post_id, username from comment c join user u on c.author_id = u.id where post_id is {id}'
+            ).fetchall()
     
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html', posts=posts, listofcomments=listofcomments)
 
 @bp.route('/blog/create', methods=('GET', 'POST'))
 @login_required
@@ -46,12 +119,6 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
-
-@bp.route('/blog/post/<int:id>')
-def post(id):
-    posts = [get_post(id, check_author=False)]
-    
-    return render_template('blog/index.html', posts=posts)
 
 def get_post(id, check_author=True):
     post = get_db().execute(
--- openbook/schema.sql
+++ openbook/schema.sql
@@ -1,9 +1,12 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS comment;
 DROP TABLE IF EXISTS note;
 DROP TABLE IF EXISTS movie;
 DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS list;
+DROP TABLE IF EXISTS list_item;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,6 +20,19 @@ CREATE TABLE post (
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+/* parent comment? */
+/* parent post  */
+
+CREATE TABLE comment (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  post_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  body TEXT NOT NULL,
+  FOREIGN KEY (post_id) REFERENCES post (id),
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
@@ -81,10 +97,42 @@ CREATE TABLE game (
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
+CREATE TABLE list (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  
+
+  title TEXT NOT NULL,
+  item TEXT NOT NULL, -- you can't have "lists or arrays" in sqlite, you can have an infinite amount of entries though, have this as a seperate table
+  price REAL,
+  isComplete BOOLEAN,
 
 
 
 
+  developer TEXT,
+  year INTEGER, 
+  genres TEXT,
+  series TEXT,
+  vibes TEXT,
+  tags TEXT,
+  setting TEXT, 
+  country TEXT, 
+  platforms TEXT,
+  image TEXT,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+
+CREATE TABLE list_item (
+
+
+
+
+
+);
 
 
 
